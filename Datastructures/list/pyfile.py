f2fa1b8dac6eb4bd4330db6b7206bd087416dd1c
brij2804 <brij2804@users.noreply.github.com>
2018-05-06 10:25:27 +0530

    first list methods

---
+++ Datastructures/list/pyfile.py
@@ -0,0 +1,5 @@
+fruits = ['orange','apple','apple','banana','pears','kiwi','banana']
+print(fruits.count('apple'))
+print(fruits.count('tangerine'))
+print(fruits.count('banana'))
+print(fruits.index('banana',4))
